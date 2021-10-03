d7ec09777360cc51a5357f87667bd7bf0a8140c4
Jiaqi <lijiaqi0412@gmail.com>
2021-10-03 15:33:10 -0400

    Set up React

---
+++ pc-shop-front-end/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Nav/>
+        <p>
+          Welcome to the PC Shop
+        </p>
+        
+      
+    </div>
+  );
+}
+
+export default App;
