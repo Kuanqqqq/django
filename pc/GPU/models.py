a991c5a60d71cb7c1c961568e49139df59b96dbd
Kuanqqq <qiaok@mymacewan.ca>
2021-10-04 17:58:37 -0600

    Modify Models

--- pc/GPU/models.py
+++ pc/GPU/models.py
@@ -4,15 +4,32 @@ from manufacturer.models import Brand
 from specifics.models import Memory, Color, TDP
 # Create your models here.
 
+
+class Chipset(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.brand.name
+
+
+class CoreClock(models.Model):
+    clock = models.FloatField()
+    unit = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.clock + ' ' + self.unit
+
+
 class GPU(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    model = models.CharField(max_length=200)
     memory = models.ForeignKey(Memory, null=True, blank=True, on_delete=models.SET_NULL)
     color = models.ForeignKey(Color, null=True, blank=True, on_delete=models.SET_NULL)
     length = models.CharField(max_length=30, null=True, )
     TDP = models.ForeignKey(TDP, null=True, on_delete=models.SET_NULL)
+    core_clock = models.ForeignKey(CoreClock, related_name='core_clock', null=True, on_delete=models.SET_NULL)
+    boost_clock = models.ForeignKey(CoreClock, related_name='boost_clock', null=True, on_delete=models.SET_NULL)
+    chipset = models.ForeignKey(Chipset, null=True, on_delete=models.SET_NULL)
+
 
     def __str__(self):
-        return self.brand.name + ' ' + self.model
-
-
+        return self.brand.name + ' ' + self.chipset.name
